5a48abb4554749cabca6fce4cdcaec1f6a9c74fb
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-09-19 09:54:17 +0200

    rgrdgdfgdsrgtaegrgdfgsdrgdgdfgewaz

--- Programowanie/nr1/nr1.cpp
+++ Programowanie/nr1/nr1.cpp
@@ -1,12 +1,20 @@
 // nr1.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
-
 #include <iostream>
-
-int main()
+int main()      
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello FORTNITEEEE!Hello\t FORTNITEEEE!Hello \tFORTNITEEEE!Hello\t FORTNITEEEE!\n"; //sta³a ³añcuchowa
     std::cout << "Gregory Kot\n";
+    ; std::cout << 'x'; //sta³a znakowa
+    std::cout << '\n';
+    std::cout << 420; //sta³a ca³kowita liczbowa
+    std::cout << '\n'
+        ;
+    std::cout << -9.6; // sta³a rzeczywista (.)
+    std::cout << '\n';
+    std::cout << "ajajaja!\n";
+    std::cout << "w twoich oczach\t";   // tab-\t enter-\n
+    std::cout << "fornitooooooooooo\n";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
@@ -18,5 +26,4 @@ int main()
 //   3. Use the Output window to see build output and other messages
 //   4. Use the Error List window to view errors
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
-    
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file    jajkopieczone.exe
