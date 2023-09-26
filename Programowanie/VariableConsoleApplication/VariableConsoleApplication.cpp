83ee51edee2ea0371c975be7e9b62c51b44989f8
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-09-26 09:46:35 +0200

    69

--- Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
+++ Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -4,13 +4,65 @@
 int main()
 {
     std::cout << "Hello World!\n";
+    std::cout << " Give\tme\ta\tfucking\tnumber\t? \n ";
+    int number_from_user;
+    std::cin >> number_from_user;
+
+    std::cout << "you gave\t" << number_from_user << "\n";
+
+    std::cout << "Hello World!\n";
+    std::cout << " Give\tme\ta\tfucking\tnumber\t? \n ";
+    int number_From_user;
+    std::cin >> number_From_user;
+
+    std::cout << "you gave\t" << number_From_user << "\n";
 }
+
 /*
+
 Algorytm - skoñczony zbiór instrukcji, który rozwi¹zuje zadany problem.
 Okreœla te¿ kolejnoœæ wykonywania dzia³añ
-Zapis algorytmu
--rysunki
+Zapis algorytmu:
+- rysunki
+- opis s³owny
+- w punktach
+- schemat blokowy
+- kod ¿ród³owy danego jêzyka programowania
+- pseudokod
 
 
+Zmienna - pewien obszar w pamiêci operacyjnej, w której mo¿na w danej chwili przechowaæ tylko 1 dan¹.
+
+
+Instrukcja deklaracji zmiennej:
+typ_zmiennej nazwa_zmiennej;
+
+typ_zmiennej - wielkoœæ obszaru pamiêci, interpretacja ci¹gu bitów
+int - 4 bajtowa liczba ca³kowita ze znakiem < -2 147 483 648, 2 147 483 647 >
+short - 2 bajtowa liczba ca³kowita ze znakiem < -32 768, 32 767 >
+long - 4 bajtowa liczba ca³kowita ze znakiem < -2 147 483 648, 2 147 483 647 >
+long long - 8 bajtowa liczba ca³kowita ze znakiem < -9 223 372 036 854 775 808, 9 223 372 036 854 775 807 >
+
+unsigned - zmienna bez znaku < 0, 2*max + 1 >
+
+
+float - 4 bajtowa liczba rzecvzywista, dok³adnoœæ 6-7 cyfr po przecinku
+double - 8 bajtowa liczba rzecvzywista, dok³adnoœæ 15-16 cyfr po przecinku
+long double - 12 bajtowa liczba rzecvzywista, dok³adnoœæ 19-20 cyfr po przecinku
+
+nazwa_zmiennej - nazwa obszaru pamiêci, identyfikator
+warunki niezbêdne:
+ - dozwolone znaki:
+    *alfabet angielski Aa-Zz
+    * cyfry arabskie 0-9
+    * podreœlenie, pod³oga( _ )
+ - nie mo¿na zacz¹æ cyfr¹
+ - unikalny w swoim zakresie widocznoœci
+ - nie mo¿e to byc s³owo kluczowe danego jêzyka
+
+Warunki programistów:
+ - nazwa zmiennej plowinna oddawaæ charakter przechowywanych danych
+ - jeœli nazwa sk³ada siê z wielu znaków w miejscu spacji podreœlenie lub od 2 s³owa pocz¹tek z du¿ej
+ - nazwa po angielsku
 
 */
