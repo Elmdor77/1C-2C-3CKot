a279684016a4d9d938c9babccb5eb801d941aad4
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-10-16 16:01:38 +0200

    if part2

--- Programowanie/Instruction_IF/Instruction_IF.cpp
+++ Programowanie/Instruction_IF/Instruction_IF.cpp
@@ -6,7 +6,7 @@ Operatory warunkowe:
 >= - wiêksze lub równe
 <= - mniejsze lub równe
 == - równe
-/= - ró¿ne
+!= - ró¿ne
 */
 
 
@@ -16,24 +16,41 @@ Operatory warunkowe:
 //Napisz program sprawdzaj¹cy czy liczba jest dodatnie czy nie.
 void task1()
 {
-	int number;
+	int number1;
 	std::cout << "Give a number:\n";
-	std::cin >> number;
-	if (number > 0)
+	std::cin >> number1;
+	if (number1 > 0)
 		std::cout << "Number is positive\n";
-	if (number < 0)
+	if (number1 < 0)
 		std::cout << "Number is negative\n";
-	if (number == 0)
+	if (number1 == 0)
 		std::cout << "Number is equal to 0\n";
 
 
 
 }
-
-
+//Napisz program sprawdzaj¹cy czy liczba jest parzysta czy nieparzysta
+void task2()
+{
+	int number2, rest;
+	std::cout << "Give a number:\n";
+	std::cin >> number2;
+	rest = number2 % 2;
+	/*
+	if (rest == 0)
+		std::cout << "Number is even\n";
+	if (rest != 0) //rest == 1
+		std::cout << "Number is odd\n";
+	*/
+	if (rest == 0)
+		std::cout << "Number is even\n";
+	else
+		std::cout << "Number is odd\n";
+}
 
 int main()
 {
-	task1();
+	//task1();
+	task2();
 }
 
