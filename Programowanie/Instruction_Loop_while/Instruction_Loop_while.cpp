2e84af3b82f4062e1edb1efd19642681ad46a7d3
Uczeń - sala 213 <Ucze - sala 213@213-13>
2023-11-28 09:35:14 +0100

    fweoi;

--- Programowanie/Instruction_Loop_while/Instruction_Loop_while.cpp
+++ Programowanie/Instruction_Loop_while/Instruction_Loop_while.cpp
@@ -40,10 +40,55 @@ void task2()
 		while (first_number % NWD != 0 || second_number % NWD != 0)
 			NWD--;
 	}
-	std::cout << "Greatest common divider = " << NWD;
+	std::cout << "Greatest common divider = " << NWD <<"\n";
+}
+void task3()
+{
+	int first_number, second_number, NWD, devinded;
+	std::cout << "Give first number\n";
+	std::cin >> first_number;
+	std::cout << "Give second number\n";
+	std::cin >> second_number;
+	NWD = 1;
+	devinded = 2;
+	if (first_number == 0 || second_number == 0)
+	{
+		if (first_number == 0)
+			NWD = second_number;
+		else if (second_number == 0)
+			NWD = first_number;
+	}
+	else
+	{
+		while (second_number >= devinded && first_number >= devinded)
+		{
+			if (second_number % devinded == 0 && first_number % devinded == 0)
+			{
+				second_number /= devinded;
+				first_number /= devinded;
+				NWD *= devinded;
+			}
+			else
+				devinded++;
+		}
+	}
+	std::cout << "Greatest common divider = " << NWD << "\n";
+}
+void task4()
+{
+	int first_number, second_number, NWD;
+	while (second_number != 0)
+	{
+		int tmp_first_number = first_number;
+		first_number = second_number;
+		second_number = tmp_first_number % second_number;
+	
+
+	}
 }
 int main()
 {
 	//task1();
-	task2();
+	//task2();
+	task3();
 }
