1f09e3e2fc8357d8b7165dfec10b31cf24116fe8
q <q>
2025-04-09 12:03:27 +0200

    u

--- Programowanie 1 kl/Matura Czerwiec 2022/Matura Czerwiec 2022.cpp
+++ Programowanie 1 kl/Matura Czerwiec 2022/Matura Czerwiec 2022.cpp
@@ -21,24 +21,25 @@ int main()
 	std::cout << "\n";
 
 	std::cout << "Zadanie 4.1:\n";
-	
+	int num_reversed = 0;
 	for (int num : numbers)
 	{
 		int tmp_num = num;
 		int tmp_num2 = num;
-		int num_reversed = 0;
+
 		if (tmp_num >= 10)
 		{
 			tmp_num = tmp_num / 10;
-			num_reversed = num_reversed + tmp_num;
-			while (tmp_num > 10)
+			num_reversed = num - 10 * tmp_num + tmp_num;
+			std::cout << num_reversed;
+			do 
 			{
 				std::cout << tmp_num2;
 				tmp_num2 = tmp_num2 - 10 * tmp_num;
 				tmp_num = tmp_num / 10;
 				
-			}
+			} while (tmp_num > 10);
 		}
-		std::cout << num_reversed<<"\n";
+		//std::cout << num_reversed<<"\n";
 	}
 }
