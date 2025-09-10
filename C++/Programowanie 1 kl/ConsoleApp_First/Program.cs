8edcac0b2592daa271c309ce51957c51282162fb
Sala213 <Sala213@ezse.pl>
2025-09-10 16:04:15 +0200

    jhbh

--- C++/Programowanie 1 kl/ConsoleApp_First/Program.cs
+++ C++/Programowanie 1 kl/ConsoleApp_First/Program.cs
@@ -1,4 +1,27 @@
-﻿Console.Write("hello World&$@*?,0000000000");
-Console.WriteLine("Hello, World!");
-int name = int.Parse(Console.ReadLine());
+﻿Console.WriteLine("Hello, World!");
+string name = Console.ReadLine();
 Console.WriteLine(name);
+string surname = Console.ReadLine();
+Console.WriteLine(surname);
+Console.WriteLine("Grah {0} {1} Grah", name, surname);
+Console.WriteLine($"Grah {name} {surname} Grah");
+Console.WriteLine(@"Prędkość 67\km\musztarde");
+Console.WriteLine("Prędkość 67\\km\\musztarde");
+
+int firstNumber = 6;
+int secondNumber = firstNumber;
+secondNumber++;
+Console.WriteLine(firstNumber);
+Console.WriteLine(secondNumber);
+
+void firstParamentTest(int p)
+{
+    Console.WriteLine(p);
+    p++;
+    Console.WriteLine(p);
+}
+
+firstNumber = 21;
+firstParamentTest(firstNumber);
+Console.WriteLine(firstNumber);
+Console.WriteLine();
