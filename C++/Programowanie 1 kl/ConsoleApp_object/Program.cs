95c28e4df3c1342c0f7aeeade2d14e2c375d4d21
Sala213 <Sala213@ezse.pl>
2025-09-18 16:53:06 +0200

    boma boom boom chiao

--- C++/Programowanie 1 kl/ConsoleApp_object/Program.cs
+++ C++/Programowanie 1 kl/ConsoleApp_object/Program.cs
@@ -35,12 +35,41 @@ Console.WriteLine($"FirstPointStruct({firstPointStruct.x}, {firstPointStruct.y})
 //-----------------------------------------------------------------
 
 PointClass firstPointClass = new PointClass();
-//TWORZY ODWOŁANIE DO ADRESU OBIEKTU
+//TWORZY ZMIENNA ODWOLUJACA DO ADRESU OBIEKTU
 firstPointClass.x = 6;
 firstPointClass.y = 7;
 Console.WriteLine($"FirstPointClass({firstPointClass.x}, {firstPointClass.y})");
 PointClass secondPointClass = firstPointClass;
 //KOPIUJE ODWOŁANIE DO OBIEKTU NIE OBIEKT
-secondPointClass.x = 7; //PONIEWAZ KOPIUJE ADRES OBIEKTU ZMIENI ZECZY W TYM OBIEKCIE (WIEC TO SAMO CO W 1 CLASIE)
+secondPointClass.x = 7; //PONIEWAZ KOPIUJE ADRES OBIEKTU ZMIENI RZEC ZY W TYM OBIEKCIE (WIEC TO SAMO CO W 1 CLASIE)
 Console.WriteLine($"SecondPointClass({secondPointClass.x}, {firstPointClass.y})");
 Console.WriteLine($"FirstPointClass({firstPointClass.x}, {firstPointClass.y})");
+
+void ParametrTestClass_v1(PointClass pc)
+{
+    Console.WriteLine($"ParametrTestClass_v1({pc.x}, {pc.y})");
+    pc.x = 76;
+    Console.WriteLine($"ParametrTestClass_v1({pc.x}, {pc.y})");
+}
+ParametrTestClass_v1(firstPointClass);
+Console.WriteLine($"ParametrTestClass_v1({firstPointClass.x}, {firstPointClass.y})");
+
+void ParametrTestClass_v2(PointClass pc)
+{
+    Console.WriteLine($"ParametrTestClass_v2({pc.x}, {pc.y})");
+    pc = new PointClass();
+    pc.x = 67;
+    Console.WriteLine($"ParametrTestClass_v2({pc.x}, {pc.y})");
+}
+ParametrTestClass_v2(firstPointClass);
+Console.WriteLine($"ParametrTestClass_v2({firstPointClass.x}, {firstPointClass.y})");
+
+void ParametrTestClass_v3(ref PointClass pc)
+{
+    Console.WriteLine($"ParametrTestClass_v3({pc.x}, {pc.y})");
+    pc = new PointClass();
+    pc.x = 82;
+    Console.WriteLine($"ParametrTestClass_v3({pc.x}, {pc.y})");
+}
+ParametrTestClass_v3(ref firstPointClass);
+Console.WriteLine($"ParametrTestClass_v3({firstPointClass.x}, {firstPointClass.y})");
