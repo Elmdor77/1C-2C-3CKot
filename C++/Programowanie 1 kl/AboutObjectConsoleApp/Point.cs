2b4028705e58f633278358ebd91e643b2ae09aae
Sala213 <Sala213@ezse.pl>
2025-09-22 13:15:04 +0200

    nothamer

---
+++ C++/Programowanie 1 kl/AboutObjectConsoleApp/Point.cs
@@ -0,0 +1,35 @@
+﻿namespace AboutObjectConsoleApp
+{
+    internal class Point
+    {
+        private int x;
+        private int y;
+
+        public Point()
+        {
+            x = 6; 
+            y = 7;
+        }
+
+        public Point(int a, int b)
+        {
+            x = a;
+            y = b;
+        }
+
+        public void Show()
+        {
+            Console.WriteLine($"({x}, {y})");
+        }
+
+        public void SetX(int x)
+        {
+            this.x = x;
+        }
+
+        public int getX()
+        {
+            return x;
+        }
+    }
+}
---
+++ C++/Programowanie 1 kl/AboutObjectConsoleApp/Program.cs
@@ -0,0 +1,11 @@
+﻿using AboutObjectConsoleApp;
+
+Point firstPoint = new Point();
+Point secondPoint = new Point(8, 9);
+
+firstPoint = new Point(8, 9);
+secondPoint = new (6, 7);
+
+firstPoint.Show();
+firstPoint.SetX(-41);
+firstPoint.Show();
