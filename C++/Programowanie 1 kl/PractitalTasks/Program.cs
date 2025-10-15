66b5058f91b9d7a09d24032c758ccd169de40717
Sala213 <Sala213@ezse.pl>
2025-10-15 15:58:27 +0200

    2023_01 styczen

--- C++/Programowanie 1 kl/PractitalTasks/Program.cs
+++ C++/Programowanie 1 kl/PractitalTasks/Program.cs
@@ -4,8 +4,8 @@ using PractitalTasks.Styczeń_2023_2;
 
 
 
-//Task1 task1 = new Task1();
-//task1.Task();
+Task1 task1 = new Task1();
+task1.Task();
 
-Task2 task2 = new Task2();
-task2.Task();
+//Task2 task2 = new Task2();
+//task2.Task();
--- C++/Programowanie 1 kl/PractitalTasks/Styczeń 2023_1/Task1.cs
+++ C++/Programowanie 1 kl/PractitalTasks/Styczeń 2023_1/Task1.cs
@@ -6,32 +6,38 @@ internal class Task1
     {
         Console.WriteLine("Podaj pierwszą liczbe");
         int a = int.Parse(Console.ReadLine());
+        if (a < 0)
+            a = a * (-1);
         Console.WriteLine("Podaj drugą liczbe");
         int b = int.Parse(Console.ReadLine());
-        SprawdzNWD(a, b);
+        if (b < 0)
+            b = b * (-1);
+        a = NWD(a, b);
         Console.WriteLine("NWD podanych liczb:");
         Console.WriteLine(a);
     }
-    private int SprawdzNWD(int a, int b)
+
+    /* *****************************************
+    nazwa funkcji: NWD
+    opis funkcji: Pobiera dwie liczby typu int i odnajduje ich NWD
+    parametry:  a - pierwsza liczba sprawdzana
+                b - druga liczba sprawdzana
+    zwracany typ i opis:  int, zmienna a (pierwsza liczba przekształcona na NWD zmiennych a oraz b
+    autor: 01234567890
+    *********************************************
+    */
+    private int NWD(int a, int b)
     {
-        while (a!=b)
+        while (a != b)
         {
             if (a > b)
             {
-                a = a - b;
+                a -= b;
             }
             else
-                b = b - a;
+                b -= a;
         }
         return a;
     }
 }
 
-/*
- nazwa funkcji: <SprawdzNWD>
-opis funkcji: <Pobiera dwie liczby typu int i odnajduje ich NWD>
-parametry: <a - pierwsza liczba sprawdzana>
-           <b - druga liczba sprawdzana>
-zwracany typ i opis:  <int, zmienna a (pierwsza liczba przekształcona na NWD zmiennych a oraz b>
-autor: <01234567890>
- */
