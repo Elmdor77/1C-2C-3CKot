5e18d4a3447ac7a8869da7f1133830118c9127d7
Elmdor77 <elmdor.kot@gmail.com>
2025-11-10 14:13:53 +0100

    grah zad 5

--- C++/Programowanie 1 kl/PractitalTasks/Program.cs
+++ C++/Programowanie 1 kl/PractitalTasks/Program.cs
@@ -1,6 +1,7 @@
 ﻿using PractitalTasks.Styczeń_2023_1;
 using PractitalTasks.Czerwiec_2022_6;
 using PractitalTasks.Czerwiec_2023_6;
+using PractitalTasks.Styczeń_2025_1;
 
 
 
@@ -14,5 +15,9 @@ using PractitalTasks.Czerwiec_2023_6;
 //Task3 task3 = new Task3();
 //task3.Task();
 
-Task1_5 task1_5 = new Task1_5();
-task1_5.Task();
+//Task1_5 task1_5 = new Task1_5();
+//task1_5.Task();
+
+
+Task4 task4 = new Task4();
+task4.Task();
---
+++ C++/Programowanie 1 kl/PractitalTasks/Styczeń 2025_1/Task4.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PractitalTasks.Styczeń_2025_1;
+
+class urządzenie
+{
+    public void wyświetl_komunikat(string komunikat )
+    {
+        Console.WriteLine(komunikat);
+    }
+}
+
+class pralka : urządzenie
+{
+    private int nr_prania = 0;
+
+    public int ustaw_pranie(int nr_prania)
+    {
+        if (nr_prania > 0  && nr_prania <= 12)
+        {
+            Console.WriteLine("Program został ustawiony");
+            this.nr_prania = nr_prania;
+        }
+        else
+        {
+            Console.WriteLine("Podano niepoprawny numer programu");
+            this.nr_prania = 0;
+        }
+            return nr_prania;
+    }
+}
+class odkurzacz : urządzenie
+{
+    private bool stan_odkurzacza = false;
+
+    public void on()
+    {
+        if (stan_odkurzacza == false)
+        {
+            stan_odkurzacza = true;
+            wyświetl_komunikat("Odkurzacz włączono");
+
+        }
+    }
+    public void off()
+    {
+        if (stan_odkurzacza == true)
+        {
+            stan_odkurzacza = false;
+            wyświetl_komunikat("Odkurzacz wyłączono");
+
+        }
+    }
+
+}
+internal class Task4
+{
+    public void Task()
+    {
+        int nr_prania = 0;
+        pralka pralka = new pralka();
+
+        Console.WriteLine("Podaj numer prania 1...12");
+        nr_prania = int.Parse(Console.ReadLine());
+        pralka.ustaw_pranie(nr_prania);
+        Console.WriteLine("Podaj numer prania 1...12");
+        nr_prania = int.Parse(Console.ReadLine());
+        pralka.ustaw_pranie(nr_prania);
+
+
+        odkurzacz odkurzacz = new odkurzacz();
+
+        odkurzacz.on();
+        odkurzacz.on();
+        odkurzacz.on();
+        odkurzacz.wyświetl_komunikat("Odkurzacz wyładował się");
+        odkurzacz.off();
+    }
+}
